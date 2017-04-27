3a325695aad758dec884e75fb9ee3bfa3177b0fb
Heidi <heidiaclayton@yahoo.com>
2017-04-27 11:23:39 -0700

    Add assignment 2 outline.

---
+++ assignments/a2/sstf-iosched.c
@@ -0,0 +1,83 @@
+/*
+ * elevator sstf
+ */
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
+#include <linux/bio.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+
+struct sstf_data {
+	struct list_head queue;
+};
+
+static void sstf_merged_requests(struct request_queue *q, struct request *rq,
+				 struct request *next)
+{
+	list_del_init(&next->queuelist);
+}
+
+static int sstf_dispatch(struct request_queue *q, int force)
+{
+
+}
+
+static void sstf_add_request(struct request_queue *q, struct request *rq)
+{
+
+}
+
+static struct request *
+sstf_former_request(struct request_queue *q, struct request *rq)
+{
+
+}
+
+static struct request *
+sstf_latter_request(struct request_queue *q, struct request *rq)
+{
+
+}
+
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
+{
+
+}
+
+static void sstf_exit_queue(struct elevator_queue *e)
+{
+
+}
+
+static struct elevator_type elevator_sstf = {
+	.ops.sq = {
+		.elevator_merge_req_fn		= sstf_merged_requests,
+		.elevator_dispatch_fn		= sstf_dispatch,
+		.elevator_add_req_fn		= sstf_add_request,
+		.elevator_former_req_fn		= sstf_former_request,
+		.elevator_latter_req_fn		= sstf_latter_request,
+		.elevator_init_fn		= sstf_init_queue,
+		.elevator_exit_fn		= sstf_exit_queue,
+	},
+	.elevator_name = "sstf",
+	.elevator_owner = THIS_MODULE,
+};
+
+static int __init sstf_init(void)
+{
+	return elv_register(&elevator_sstf);
+}
+
+static void __exit sstf_exit(void)
+{
+	elv_unregister(&elevator_sstf);
+}
+
+module_init(sstf_init);
+module_exit(sstf_exit);
+
+
+MODULE_AUTHOR("Team 11-01");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("SSTF IO scheduler");
