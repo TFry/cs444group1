69ffb9d1462616fe647ae042a102dccb2f1a24a9
heidiaclayton <heidiaclayton@gmail.com>
2017-05-03 22:17:56 -0700

    Concurrency modifications.

--- assignments/a2/concurrency.py
+++ assignments/a2/concurrency.py
@@ -23,15 +23,15 @@ class Philosopher (threading.Thread):
         while self.dining:
             # Sleeps from 1 to 20 seconds
             self.think()
-            print(self.name + " is hungry")
+            print(self.name + " is hungry.\n")
             # Tries to acquire forks (locks)
             self.get_forks()
             # Eats from 2 to 9 seconds
             self.eat()
             # Releases forks (locks)
-            print("RELEASE FORKS")
+            print("RELEASE FORKS.\n")
             self.put_forks()
-            print(self.name + " is now done eating. Going to go think now.")
+            print(self.name + " is now done eating. Going to go think now.\n")
     
     def think(self):
         time.sleep(random.randrange(1,20))
@@ -45,15 +45,17 @@ class Philosopher (threading.Thread):
                 break
             else:
                 # Release the first fork, sleep, and try again
+                self.left_fork.acquire(False)
                 self.left_fork.release()
-        else:
-            exit
+
     def eat(self):
-            print(self.name + " is eating his meal.")
+            print(self.name + " is eating his meal.\n")
             time.sleep(random.uniform(2,9))
 
-    def put_forks(self): 
+    def put_forks(self):
+        self.left_fork.acquire(False)
         self.left_fork.release()
+        self.right_fork.acquire(False)
         self.right_fork.release()
 
 
@@ -64,7 +66,7 @@ def main():
     # Iterate 5 times and instantiate the Philospher (thread) class
     # (0,1) (1,2) (2,3) (3,4) (4,0) <- Fork pairs
     #  x % 5, (x+1) % 5 <-  This is how we get the number pairs above
-    for x in range(0,5):
+    for x in range(0, 5):
         thread = Philosopher(x, names[x], forks[x % 5], forks[(x+1) % 5])
         threads.append(thread)
         threads[x].start()
@@ -73,14 +75,7 @@ def main():
     # After runtime is depleted, set their dining variable to False so the threads stop running
     for t in threads:
         t.dining = False
-    print "Exiting Main Thread"
-    
-    
-
-
-
-
-
+    print "Exiting Main Thread.\n"
 
 main()
 
