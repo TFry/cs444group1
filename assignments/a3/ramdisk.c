ff2034b966448610eaf7816ce7e353f72efa780f
heidiaclayton <heidiaclayton@gmail.com>
2017-05-21 02:53:57 -0700

    Add cryptography

--- assignments/a3/ramdisk.c
+++ assignments/a3/ramdisk.c
@@ -1,9 +1,8 @@
 /*
- * A sample, extra-simple block driver. Updated for kernel 2.6.31.
- *
- * (C) 2003 Eklektix, Inc.
- * (C) 2010 Pat Patterson <pat at superpat dot com>
- * Redistributable under the terms of the GNU GPL.
+ * heavily referenced from http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
+ * to at least get it to compile
+ * referenced http://stackoverflow.com/questions/8927827/encryption-and-decryption-using-aes-256
+ * and man pages for cryptography
  */
 
 #include <linux/module.h>
@@ -18,7 +17,6 @@
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/hdreg.h>
-#include <linux/crypto.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 static char *Version = "1.4";
@@ -28,77 +26,24 @@ module_param(major_num, int, 0);
 static int logical_block_size = 512;
 module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
-module_param(nsectors, int, 0600);
+module_param(nsectors, int, 0);
 
-//Crypto stuff
-#define KEY_SIZE 32 /* AES has a maximum key size of 256 bits */
-static char crypto_key[KEY_SIZE];
-static int key_size = 0; /* size of the current key */
-/*
- * We can tweak our hardware sector size, but the kernel talks to us
- * in terms of small sectors, always.
- */
 #define KERNEL_SECTOR_SIZE 512
 
-/*
- * Our request queue.
- */
+#define KEY_SIZE 32
+static char *crypto_key = "cs444hw3";
+
 static struct request_queue *Queue;
 
-//Base cipher struct
-/*
- * The internal representation of our device.
- */
 static struct sbd_device {
 	unsigned long size;
 	spinlock_t lock;
 	u8 *data;
 	struct gendisk *gd;
+        // variables for crytography
+        struct crypto_cipher *cipher;
+        struct scatterlist encrypt[2];
 } Device;
-
-struct crypto_cipher *tfm;
-
-//ssize_t: This data type is used to represent the 
-//sizes of blocks that can be read or written in a
-// single operation. It is similar to size_t , but 
-// must be a signed type.
-
-ssize_t key_display(struct device *dev, struct device_attribute *attr, char *buf)
-{	
-	//Printing out we are copying the key
-	//Formating the crypto key string and placing 
-	//it into a buffer with scnprintf
-	printk(KERN_DEBUG "sbd: copying the key\n");
-	return scnprintf(buf, PAGE_SIZE, "%s\n", crypto_key);
-}
-
-//Stores the key into the buffer
-//first it checks that the count size is not invalid
-//then formats the key and puts it into the buffer
-ssize_t key_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count){
-	if(count != 16 && count != 24 && count != 32){
-		printk(KERN_WARNING "sbd: error, key size is invalid %lu\n", (unsigned long)count);
-		return -EINVAL;
-	}
-	printk(KERN_DEBUG "sbd: storing the cipher key \n");
-	//formats the crypto key into a printf format and stores 
-	//the key in the buffer location pointed to by crypto_key
-	snprintf(crypto_key, sizeof(crypto_key), "%.*s", (int)min(count, sizeof(crypto_key) - 1),buf);
-	key_size = count;
-	return count;
-}
-
-DEVICE_ATTR(key, 0600, key_display, key_store);
-
-static void rd_root_dev_release(struct device *dev)
-{
-}
-
-// sysfs
-static struct device rd_root_dev = {
-	.init_name = "sbd",
-	.release = rd_root_dev_release,
-};
 
 /*
  * Handle an I/O request.
@@ -107,53 +52,30 @@ static void sbd_transfer(struct sbd_device *dev, sector_t sector,
 		unsigned long nsect, char *buffer, int write) {
 	unsigned long offset = sector * logical_block_size;
 	unsigned long nbytes = nsect * logical_block_size;
-	
-	int k;
+        unsigned long i;
+	char *blocks = dev->data + offset;
+        unsigned long size = crypto_cipher_blocksize(dev->cipher);
 
-	if(key_size == 0){
-		printk(KERN_INFO "no key set\n");
-	}else{
-		crypto_cipher_clear_flags(tfm, ~0);
-		crypto_cipher_setkey(tfm, crypto_key, key_size);
-	}
-	
 	if ((offset + nbytes) > dev->size) {
 		printk (KERN_NOTICE "sbd: Beyond-end write (%ld %ld)\n", offset, nbytes);
 		return;
 	}
-	//We gonna do some shit here
-	//Check the key size is not 0
-	///loop through while k is not the size nbytes
-	//encrypt one data entry with the offset of the size
-	//apply it to the buffer with that size as the offset
 	if (write)
-	{
-		if(key_size != 0)
+        {
+                // as described in man page, this only encrypts a single block, so we 
+                // have to go block by block or encryption/decryption will do weird stuff
+                for (i = 0; i < nbytes; i += size)
 		{
-			for(k=0; k < nbytes; k+= crypto_cipher_blocksize(tfm))
-			{
-				crypto_cipher_encrypt_one(tfm, dev->data+offset+k, buffer+k);
-			}
-		}
-		else
-		{
-			memcpy(dev->data + offset, buffer, nbytes);
-		}
-	}
+                crypto_cipher_encrypt_one(dev->cipher, &blocks[i], &buffer[i]);
+                }
+        }
 	else
-	{
-		if(key_size != 0)
+        {
+                for (i = 0; i < nbytes; i += size)
 		{
-			for (k = 0; k < nbytes; k+= crypto_cipher_blocksize(tfm)) 
-			{
-				crypto_cipher_decrypt_one(tfm, buffer+k, dev->data+offset+k);
-			}
-		}
-		else
-		{
-			memcpy(buffer, dev->data + offset, nbytes);
-		}
-	}
+                crypto_cipher_decrypt_one(dev->cipher, &blocks[i], &buffer[i]);
+                }
+        }
 }
 
 static void sbd_request(struct request_queue *q) {
@@ -203,22 +125,6 @@ static struct block_device_operations sbd_ops = {
 };
 
 static int __init sbd_init(void) {
-	int ret;
-	
-	tfm = crypto_alloc_cipher("aes",0,16);
-
-	if (IS_ERR(tfm)){
-		printk(KERN_ERR "cipher: transformation failed");
-		return PTR_ERR(tfm);
-	}	
-
-	Device.size = nsectors * logical_block_size;
-	spin_lock_init(&Device.lock);
-	Device.data = vmalloc(Device.size);
-	if (Device.data == NULL) {
-		return -ENOMEM;
-	}
-
 	/*
 	 * Set up our internal device.
 	 */
@@ -227,17 +133,16 @@ static int __init sbd_init(void) {
 	Device.data = vmalloc(Device.size);
 	if (Device.data == NULL)
 		return -ENOMEM;
-	/*
-	 * Get a request queue.
-	 */
+
 	Queue = blk_init_queue(sbd_request, &Device.lock);
 	if (Queue == NULL)
 		goto out;
 	blk_queue_logical_block_size(Queue, logical_block_size);
-	/*
-	 * Get registered.
-	 */
-	major_num = register_blkdev(major_num, "sbd");
+
+        Device.cipher = crypto_alloc_cipher("aes", 4, CRYPTO_ALG_ASYNC);
+	crypto_cipher_setkey(Device.cipher, crypto_key, KEY_SIZE);
+
+        major_num = register_blkdev(major_num, "sbd");
 	if (major_num < 0) {
 		printk(KERN_WARNING "sbd: unable to get major number\n");
 		goto out;
@@ -256,17 +161,6 @@ static int __init sbd_init(void) {
 	set_capacity(Device.gd, nsectors);
 	Device.gd->queue = Queue;
 	add_disk(Device.gd);
-	
-	ret = device_register(&rd_root_dev);
-	if (ret < 0){
-		goto out_unregister;
-	}
-
-	ret = device_create_file(&rd_root_dev, &dev_attr_key);
-	if (ret < 0) {
-		device_unregister(&rd_root_dev);
-		goto out_unregister;
-	}
 
 	return 0;
 
@@ -274,7 +168,6 @@ out_unregister:
 	unregister_blkdev(major_num, "sbd");
 out:
 	vfree(Device.data);
-	crypto_free_cipher(tfm);
 	return -ENOMEM;
 }
 
@@ -284,11 +177,6 @@ static void __exit sbd_exit(void)
 	put_disk(Device.gd);
 	unregister_blkdev(major_num, "sbd");
 	blk_cleanup_queue(Queue);
-	crypto_free_cipher(tfm);	
-	device_remove_file(&rd_root_dev, &dev_attr_key);
-	device_unregister(&rd_root_dev);	
-
-
 	vfree(Device.data);
 }
 
