0b731ba665ba457b9cd2195a6280f87a6492fa90
heidiaclayton <heidiaclayton@gmail.com>
2017-05-22 00:58:25 -0700

    Edit makefile and test.

--- assignments/a3/test.sh
+++ assignments/a3/test.sh
@@ -2,7 +2,7 @@
 
 echo "Starting test.\n"
 
-echo "Setting up module, creating filesystem, and mounting.\n"
+echo "Setting up module, creating filesystem, and mounting."
 
 insmod ramdisk.ko
 shred -z /dev/sbd0
@@ -10,25 +10,25 @@ mkfs.ext2 /dev/sbd0
 lsmod
 mount /dev/sbd0 /mnt/test
 
-echo "Writing 'Testing homework 3!' to a file on mounted disk.\n"
+echo "Writing 'Testing homework 3!' to a file on mounted disk."
 
 echo "Testing homework 3!" > /mnt/test/test.txt
 
 echo "Wrote to file.\n"
 
-echo "Try to search for 'Testing homework 3!' on /dev/sbd0. Nothing should appear.\n"
+echo "Try to search for 'Testing homework 3!' on /dev/sbd0. Nothing should appear."
 
 grep -a "Testing homework 3!" /dev/sbd0
 
-echo "Result of trying to view file on /mnt/test using 'cat'\n"
+echo "Result of trying to view file on /mnt/test using 'cat'"
 
 cat /mnt/test/test.txt
 
-echo "Trying to list contents of /dev/sbd0 using cat should output garbage.\n"
+echo "Trying to list contents of /dev/sbd0 using cat should output garbage."
 
 cat /dev/sbd0
 
-echo "Umounting and removing module.\n"
+echo "Umounting and removing module."
 
 umount /mnt/test
 rmmod ramdisk.ko
